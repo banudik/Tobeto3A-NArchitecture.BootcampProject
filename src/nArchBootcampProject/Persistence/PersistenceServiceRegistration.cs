64d4ce7b7f52329ce0a6e4e9e4300ddae630618b
Banu Dik <banudik_34@hotmail.com>
2024-03-25 01:53:39 +0300

    chore: initial commit from nArchitecture.Gen

---
+++ src/nArchBootcampProject/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NArchitecture.Core.Persistence.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("BaseDb"));
+        services.AddDbMigrationApplier(buildServices => buildServices.GetRequiredService<BaseDbContext>());
+
+        services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
+        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        services.AddScoped<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
+        return services;
+    }
+}
