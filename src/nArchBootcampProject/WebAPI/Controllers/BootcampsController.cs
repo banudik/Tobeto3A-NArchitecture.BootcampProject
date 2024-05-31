bb95f402efd8b70e599235c208c322f311b207ae
Ege Umut Tali <egeumuttali@hotmail.com>
2024-06-01 01:33:20 +0300

    added new bootcamp queries

--- src/nArchBootcampProject/WebAPI/Controllers/BootcampsController.cs
+++ src/nArchBootcampProject/WebAPI/Controllers/BootcampsController.cs
@@ -4,6 +4,7 @@ using Application.Features.Bootcamps.Commands.Update;
 using Application.Features.Bootcamps.Queries.GetById;
 using Application.Features.Bootcamps.Queries.GetList;
 using Application.Features.Bootcamps.Queries.GetListByApplicationId;
+using Application.Features.Bootcamps.Queries.GetListByBootcampState;
 using Application.Features.Bootcamps.Queries.GetListByInstructorId;
 using Microsoft.AspNetCore.Mvc;
 using NArchitecture.Core.Application.Requests;
@@ -75,6 +76,14 @@ public class BootcampsController : BaseController
     public async Task<IActionResult> GetListBootcampByApplication([FromQuery] PageRequest pageRequest, Guid applicantId)
     {
         GetListByApplicationQuery query = new() { PageRequest = pageRequest, ApplicantId = applicantId };
+        var result = await Mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpGet("getbootcampbybootcampstate")]
+    public async Task<IActionResult> GetListBootcampByBootcampState([FromQuery] PageRequest pageRequest, short bootcampStateId)
+    {
+        GetListBootcampByBootcampStateQuery query = new() { PageRequest = pageRequest, BootcampStateId = bootcampStateId };
         var result = await Mediator.Send(query);
         return Ok(result);
     }
