4fd9f3e84aff1c9052fba374dce1c07542fa566b
Banu Dik <banudik_34@hotmail.com>
2024-06-29 13:32:41 +0300

    contact us / send email added, worked. (sendemailcommand,contactformmodel,contactcontroller)

---
+++ src/nArchBootcampProject/WebAPI/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Contact.SendEmail;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ContactController : BaseController
+{
+    private readonly IMediator _mediator;
+
+    public ContactController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("send-email")]
+    public async Task<IActionResult> SendEmail([FromBody] ContactFormModel model)
+    {
+        await _mediator.Send(new SendEmailCommand(model));
+        return Ok(new { message = "Email sent successfully" });
+    }
+}
+
